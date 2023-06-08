de7c3be662643ce3db902c4986bf7167f830778d
Alexey Emelchikov <aleksemelchikov@gmail.com>
2023-06-08 19:14:45 +0300

    Задание модуля 12

---
+++ Module12/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Module12
+{
+    public class Program
+    {
+        public delegate void MyDelegate();
+        static void Main(string[] args)
+        {
+            User user = new User();
+            Console.Write("Введите ваше имя ");
+            user.Name = Console.ReadLine();
+            string greetings = "Привет " + user.Name;
+            if (user.IsPremium == true)
+                return;
+            else
+            {
+                User.ShowAds();
+
+            }
+        }
+    }
+}
